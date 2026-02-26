0b7cda5041f4c3851a9fd1a5181e730147a44024
TEJAS KRISHNA YADAV <yadavtejas2806@gmail.com>
2026-02-26 11:07:41 +0530

    Add files via upload

---
+++ customers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customers(
+customer_id SERIAL,
+customer_name VARCHAR(150),
+phone_number VARCHAR(20),
+email VARCHAR(150),
+city VARCHAR(100),
+country VARCHAR(100),
+created_at TIMESTAMP
+);
